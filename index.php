64a04121b29a49aff06749c4bae14d2eb10918ff
Dylan Goncalves <dylanphx93@gmail.com>
2026-07-21 13:50:46 +0200

    first commit: 30 minutes of kata

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+/*kata-t-height
+Kata 79 per a especialitats web 3-6-26: Avui rendirem un petit tribut a la lletra "T", Donat un enter n, imprimiu "T" d'alçada n.
+
+Input: 5, Output: TTTTT   
+                    T   
+                    T  
+                    T  
+                    T  
+
+Input: 7, Output: TTTTTTT   
+                     T          
+                     T    
+                     T    
+                     T   
+                     T   
+                     T 
+
+Imput: 4, Output: TT TT    
+                    T     
+                    T    
+                    T*/     
+
+$number = readline("Introduce un Numero: ");
+        
+for ($i = 0; $i < $number; $i++) {
+     for ($j = 0; $j < $number; $j++) {
+          if ($j == 1) { 
+            
+          }
+     }
+     echo PHP_EOL;
+}
+
+
+  
+  
